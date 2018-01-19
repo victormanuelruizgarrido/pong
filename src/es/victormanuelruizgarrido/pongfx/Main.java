5bfc6c97ce90d73f81aaeba25fd459ffda0f54bd
1DAW02 <1DAW02@DESKTOP-6ATC9G3>
2018-01-19 14:34:21 +0100

    Pong terminado hasta puntuacion

--- src/es/victormanuelruizgarrido/pongfx/Main.java
+++ src/es/victormanuelruizgarrido/pongfx/Main.java
@@ -7,15 +7,18 @@ añadir*/
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
@@ -31,7 +34,10 @@ public class Main extends Application {
     final int STICK_LARGO =60;
     int stickPosiY=(SCENE_MEDIDA_Y-STICK_GROSOR)/2;
     int stickVelocidad = 0;
-    
+    int stickVelocidad2 = 0;
+    final int TEXT_SIZE = 24;
+    int score;
+    int hightScore;
     
     @Override
     public void start(Stage primaryStage) {     
@@ -49,6 +55,11 @@ public class Main extends Application {
         rectanguloEsquinas.setFill(Color.BLUE);
         rectanguloEsquinas.setArcWidth(10);
         rectanguloEsquinas.setArcHeight(10);
+        
+        Rectangle rectanguloEsquinas2 = new Rectangle(SCENE_MEDIDA_X*0.1,barraPosicionY,STICK_GROSOR,STICK_LARGO);
+        rectanguloEsquinas2.setFill(Color.BLUE);
+        rectanguloEsquinas2.setArcWidth(10);
+        rectanguloEsquinas2.setArcHeight(10);
         for(int i=0; i<SCENE_MEDIDA_Y; i+=30){
            Line lineaCentral = new Line(SCENE_MEDIDA_X/2,i,SCENE_MEDIDA_X/2,i+10);
             lineaCentral.setStroke(Color.BLUE);
@@ -60,8 +71,46 @@ public class Main extends Application {
         circleBall.setCenterY(ballCenterY);
         circleBall.setRadius(7);
         circleBall.setFill(Color.BLUE);
+        //Layout principal
+        HBox paneScores = new HBox();
+        paneScores.setTranslateY(20);
+        paneScores.setMinWidth(SCENE_MEDIDA_X);
+        paneScores.setAlignment(Pos.CENTER);
+        paneScores.setSpacing(100);
+        root.getChildren().add(paneScores);
+        //Layout puntuacion actual
+        HBox paneActualScores = new HBox();
+        paneActualScores.setSpacing(10);
+        paneScores.getChildren().add(paneActualScores);
+        //Layout puntuacion maxima
+        HBox paneMaximaScores = new HBox();
+        paneMaximaScores.setSpacing(10);
+        paneScores.getChildren().add(paneMaximaScores);
+        //Texto de etiqueta para puntuacion
+        Text textoTituloPuntuacion = new Text("Score");
+        textoTituloPuntuacion.setFont(Font.font(TEXT_SIZE));
+        textoTituloPuntuacion.setFill(Color.BLUE);
+        //Texto para puntuacion
+        Text textoPuntuacion = new Text("0");
+        textoPuntuacion.setFont(Font.font(TEXT_SIZE));
+        textoPuntuacion.setFill(Color.BLUE);
+        //Texto de etiqueta para puntuacion
+        Text textoTituloMaximaPuntuacion = new Text("Max.Score");
+        textoTituloMaximaPuntuacion.setFont(Font.font(TEXT_SIZE));
+        textoTituloMaximaPuntuacion.setFill(Color.BLUE);
+        //Texto para puntuacion
+        Text textoMaximaPuntuacion = new Text("0");
+        textoMaximaPuntuacion.setFont(Font.font(TEXT_SIZE));
+        textoMaximaPuntuacion.setFill(Color.BLUE);
+        //Añadir los textos a los layaut reservados
+        paneActualScores.getChildren().add(textoTituloPuntuacion);
+        paneActualScores.getChildren().add(textoPuntuacion);
+        paneMaximaScores.getChildren().add(textoTituloMaximaPuntuacion);
+        paneMaximaScores.getChildren().add(textoMaximaPuntuacion);
         root.getChildren().add(circleBall);
         root.getChildren().add(rectanguloEsquinas);
+        root.getChildren().add(rectanguloEsquinas2);
+        
         AnimationTimer animationBall = new AnimationTimer(){
             @Override
             public void handle(long now) {
@@ -76,10 +125,20 @@ public class Main extends Application {
                     }
                 }
                 rectanguloEsquinas.setY(stickPosiY);
+                rectanguloEsquinas2.setY(stickPosiY);
                 circleBall.setCenterX(ballCenterX);
                 ballCenterX+=ballVelocidadX;
                 if(ballCenterX>=SCENE_MEDIDA_X){
-                    ballVelocidadX=-3;
+                    if(score>hightScore){
+                        //Cambiar puntuacion mas alta
+                        hightScore=score;
+                        textoMaximaPuntuacion.setText(String.valueOf(hightScore));
+                    }
+                    //Reiniciar partida
+                    score=0;
+                    textoPuntuacion.setText(String.valueOf(score));
+                    ballCenterX = 10;
+                    ballVelocidadY = 3;
                 }
                 if(ballCenterX<=0){
                     ballVelocidadX=3;
@@ -95,18 +154,29 @@ public class Main extends Application {
                 Shape.intersect(circleBall, rectanguloEsquinas);
                 Shape shapeColision = Shape.intersect(circleBall, rectanguloEsquinas);
                 boolean colisionVacia = shapeColision.getBoundsInLocal().isEmpty();
-                if(colisionVacia == false){
+                if(colisionVacia == false && ballVelocidadX > 0){
                     //Colisionj detectada
                     ballVelocidadX=-3;
+                    //Colisionj detectada
+                    
+                    //Icrementar puntuacion actual
+                    score++;
+                    textoPuntuacion.setText(String.valueOf(score));
+                }
+                Shape.intersect(circleBall, rectanguloEsquinas2);
+                Shape shapeColision2 = Shape.intersect(circleBall, rectanguloEsquinas2);
+                boolean colisionVacia2 = shapeColision2.getBoundsInLocal().isEmpty();
+                if(colisionVacia2 == false && ballVelocidadX < 0){
+                    //Colisionj detectada
+                    ballVelocidadX=3;
+                    //Icrementar puntuacion actual
+                    score++;
+                    textoPuntuacion.setText(String.valueOf(score));
                 }
             };//Cierre de handler
             
         }; // Fin AnimationTimer
         animationBall.start();
-        scene.setOnKeyPressed((KeyEvent event) -> {
-        });
-        scene.setOnKeyReleased((KeyEvent event) -> {
-        });
         scene.setOnKeyPressed((KeyEvent event)-> {
             switch(event.getCode()){
                 case UP:
